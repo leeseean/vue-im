f3eaefe1a4d58a9400a9ee0f4c99126a30b77b53
leeseean <leeseean@qq.com>
2019-11-16 23:22:51 +0800

    upload

--- build/webpack.dev.conf.js
+++ build/webpack.dev.conf.js
@@ -28,7 +28,7 @@ const devWebpackConfig = merge(baseWebpackConfig, {
         { from: /.*/, to: path.posix.join(config.dev.assetsPublicPath, 'index.html') },
       ],
     },
-    https: true || isHttps,
+    https: isHttps,
     hot: true,
     contentBase: false, // since we use CopyWebpackPlugin.
     compress: true,
